167ee1f85377f0735bf975f7d4155f3da1f2e14f
Tomasz Kula <tomaszkula07@gmail.com>
2016-01-25 23:33:46 +0100

    creating the board and the players

--- game.rb
+++ game.rb
@@ -1,12 +1,27 @@
 class Game
 attr_accessor :player1, :player2, :active_player, :winner, :board
 
-  def initialize(plr1, plr2)
-    @player1 = plr1
-    @player2 = plr2
+  def initialize
+    @player1 = nil
+    @player2 = nil
     @active_player = @player1
     @winner = false
     @board = []
+  end
+
+  def play
+  end
+
+  def create_the_board
+    @board = [[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "]]
+  end
+
+  def create_the_players
+    puts "Enter plr1"
+    @player1 = gets.chomp
+    puts "Enter plr2"
+    @player2 = gets.chomp
+    @active_player = @player1
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,21 +1,57 @@
 require_relative "../game"
 
 describe Game do
+  let(:game) { Game.new }
   describe '#initialize' do
-    let(:game) { Game.new("Tomasz", "Piotr") }
+
     context 'when initialized' do
-      it { expect(game.player1).to eql "Tomasz" }
-      it { expect(game.player2).to eql "Piotr" }
-      it { expect(game.active_player).to eql "Tomasz" }
+      it { expect(game.player1).to eql nil }
+      it { expect(game.player2).to eql nil }
+      it { expect(game.active_player).to eql nil }
       it { expect(game.board).to eql [] }
       it { expect(game.winner).to eql false }
+    end
+  end
+
+  describe '#create_the_board' do
+    before { game.create_the_board }
+    context 'when created' do
+      it { expect(game.board.size).to eql 6 }
+      it { expect(game.board[0].size).to eql 7 }
+      it { expect(game.board[0][0]).to eql " " }
+    end
+  end
+
+  describe '#create_the_players' do
+    before do
+      io_obj = double
+      expect(subject)
+        .to receive(:gets)
+        .and_return(io_obj)
+        .twice
+      expect(io_obj)
+        .to receive(:chomp)
+        .and_return(:player1)
+      expect(io_obj)
+        .to receive(:chomp)
+        .and_return(:player2)
+    end
+
+    it 'sets @player1 and @player2 according to user\'s input' do
+      subject.create_the_players
+      expect(subject.instance_variable_get(:@player1)).to eq :player1
+      expect(subject.instance_variable_get(:@player2)).to eq :player2
+      expect(subject.instance_variable_get(:@active_player)).to eq :player1
     end
   end
 
   describe '#get_input' do
   end
 
-  describe '#check_input' do
+  describe '#correct_input?' do
+  end
+
+  describe '#empty_space?' do
   end
 
   describe '#update_board' do
